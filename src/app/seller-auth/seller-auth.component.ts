da7da209a81eb1ea38042241d0b39169e851bebc
Tushar18031998 <tushar18bharambe@gmail.com>
2023-04-13 12:44:13 +0530

    new commits

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SellerService } from '../services/seller.service';
+import { Router } from '@angular/router';
+import { Signup } from '../data-type';
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
@@ -7,11 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellerAuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(private seller:SellerService,private router:Router) { }
+  showLogin=false
 
   ngOnInit(): void {
+    this.seller.reloadSeller()
   }
-signUp(data:object):void{
+signUp(data:Signup):void{
+this.seller.userSignup(data)
+
+}
+Login(data:Signup):void{
 console.warn(data)
+  }
+openLogin(){
+this.showLogin=true
+}
+openSignUp(){
+  this.showLogin=false
 }
 }
