2efd00e64ff0a49aa7c3bc720cc53686f2a904d7
Tushar18031998 <tushar18bharambe@gmail.com>
2023-05-15 21:20:12 +0530

    new changes

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { product } from '../data-type';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  constructor(private http: HttpClient) {}
+  addProduct(data: product) {
+    console.log('Service called');
+    return this.http.post('http://localhost:3000/Products', data);
+  }
+  productList() {
+    return this.http.get<product[]>('http://localhost:3000/Products');
+  }
+  deleteProduct(id: number) {
+    return this.http.delete(`http://localhost:3000/Products/${id}`);
+  }
+  getProduct(id: string) {
+    return this.http.get<product>(`http://localhost:3000/Products/${id}`);
+  }
+  updateProduct(product:product){
+    console.log(product)
+   return this.http.put<product>(`http://localhost:3000/Products/${product.id}`,product)
+  }
+}
